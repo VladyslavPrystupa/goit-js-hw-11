bc2cc34b8fdc957bc6d386cf9b63883a37e0943e
VladyslavPrystupa <prystupa.vladyslavv@gmail.com>
2023-01-20 23:14:59 +0200

    try/catch

--- src/js/fetch.js
+++ src/js/fetch.js
@@ -1,32 +1,22 @@
-import Notiflix from 'notiflix';
 import axios from 'axios';
 
 const BASE_URL = 'https://pixabay.com/api/';
 const KEY = '32943531-cb871ea456f4d19bb7942720c';
 
 export const fetchImg = async (value, page) => {
-  try {
-    const resp = await axios.get(BASE_URL, {
-      params: {
-        key: KEY,
-        q: `${value}`,
-        image_type: 'photo',
-        orientation: 'horizontal',
-        safesearch: true,
-        per_page: 40,
-        page: `${page}`,
-      },
-    });
+  const resp = await axios.get(BASE_URL, {
+    params: {
+      key: KEY,
+      q: `${value}`,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: true,
+      per_page: 40,
+      page: `${page}`,
+    },
+  });
 
-    return resp.data;
-  } catch (err) {
-    console.log(err);
-    throw new Error(
-      Notiflix.Notify.failure(
-        'Sorry, there are no images matching your search query. Please try again.'
-      )
-    );
-  }
+  return resp.data;
 };
 
 // export const fetchImg = async (value, page) => {
--- src/js/index.js
+++ src/js/index.js
@@ -13,27 +13,35 @@ async function onSubmit(evt) {
   evt.preventDefault();
   const value = refs.form.elements.searchQuery.value;
   page = 1;
+  try {
+    const data = await fetchImg(value, page);
 
-  const data = await fetchImg(value, page);
+    totalImg = 0; // для скидання загальної к-сті картинок при новому запиті(без перезавантаженн сторінки)
+    totalImg += data.hits.length; // додаємо перші картинки на сторінці
 
-  totalImg = 0; // для скидання загальної к-сті картинок при новому запиті(без перезавантаженн сторінки)
-  totalImg += data.hits.length; // додаємо перші картинки на сторінці
+    if (data.hits.length === 0) {
+      refs.moreBtn.hidden = true;
+      Notiflix.Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+    } else {
+      refs.moreBtn.hidden = false;
+      Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
+    }
 
-  if (data.hits.length === 0) {
-    refs.moreBtn.hidden = true;
-    Notiflix.Notify.failure(
-      'Sorry, there are no images matching your search query. Please try again.'
+    if (totalImg === data.totalHits) {
+      refs.moreBtn.hidden = true;
+    } // якшо при першому запиті картинки поміщаються на одну сторінку то прибираємо кнопку
+
+    return (refs.gallery.innerHTML = markup(data.hits));
+  } catch (err) {
+    console.log(err);
+    throw new Error(
+      Notiflix.Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      )
     );
-  } else {
-    refs.moreBtn.hidden = false;
-    Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
   }
-
-  if (totalImg === data.totalHits) {
-    refs.moreBtn.hidden = true;
-  } // якшо при першому запиті картинки поміщаються на одну сторінку то прибираємо кнопку
-
-  return (refs.gallery.innerHTML = markup(data.hits));
 }
 
 async function loadMore() {
