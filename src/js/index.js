04e3b2473ad6194800207d0b99535bdd82e6f579
VladyslavPrystupa <prystupa.vladyslavv@gmail.com>
2023-01-22 21:32:01 +0200

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -10,63 +10,60 @@
 // refs.moreBtn.addEventListener('click', loadMore);
 
 // async function onSubmit(evt) {
-//   evt.preventDefault();
-//   const value = refs.form.elements.searchQuery.value;
-//   page = 1;
-
-//   const data = await fetchImg(value, page).catch(err => {
-//     console.log(err);
+//   try {
+//     evt.preventDefault();
+//     const value = refs.form.elements.searchQuery.value;
+//     page = 1;
+//     const data = await fetchImg(value, page);
+//     totalImg = 0; // для скидання загальної к-сті картинок при новому запиті(без перезавантаженн сторінки)
+//     totalImg += data.hits.length; // додаємо перші картинки на сторінці
+//     if (value === '') {
+//       return Notiflix.Notify.failure('Please enter your search query.');
+//     }
+//     if (data.hits.length === 0) {
+//       refs.moreBtn.hidden = true;
+//       Notiflix.Notify.failure(
+//         'Sorry, there are no images matching your search query. Please try again.'
+//       );
+//     } else {
+//       refs.moreBtn.hidden = false;
+//       Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
+//     }
+//     if (totalImg === data.totalHits) {
+//       refs.moreBtn.hidden = true;
+//     } // якшо при першому запиті картинки поміщаються на одну сторінку то прибираємо кнопку
+//     return (refs.gallery.innerHTML = markup(data.hits));
+//   } catch (error) {
+//     console.log(error);
 //     throw new Error(
 //       Notiflix.Notify.failure(
 //         'Sorry, there are no images matching your search query. Please try again.'
 //       )
 //     );
-//   });
-
-//   totalImg = 0; // для скидання загальної к-сті картинок при новому запиті(без перезавантаженн сторінки)
-//   totalImg += data.hits.length; // додаємо перші картинки на сторінці
-
-//   if (value === '') {
-//     return Notiflix.Notify.failure('Please enter your search query.');
 //   }
-
-//   if (data.hits.length === 0) {
-//     refs.moreBtn.hidden = true;
-//     Notiflix.Notify.failure(
-//       'Sorry, there are no images matching your search query. Please try again.'
-//     );
-//   } else {
-//     refs.moreBtn.hidden = false;
-//     Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
-//   }
-
-//   if (totalImg === data.totalHits) {
-//     refs.moreBtn.hidden = true;
-//   } // якшо при першому запиті картинки поміщаються на одну сторінку то прибираємо кнопку
-
-//   return (refs.gallery.innerHTML = markup(data.hits));
 // }
 
 // async function loadMore() {
-//   page += 1;
-//   const value = refs.form.elements.searchQuery.value;
-//   const newImg = await fetchImg(value, page).catch(err => {
-//     console.log(err);
+//   try {
+//     page += 1;
+//     const value = refs.form.elements.searchQuery.value;
+//     const newImg = await fetchImg(value, page);
+
+//     totalImg += newImg.hits.length; // додаємо до загальної к-сті нові дозавантажені картинки
+
+//     if (totalImg === newImg.totalHits) {
+//       refs.moreBtn.hidden = true;
+//       Notiflix.Notify.info(`You have got all avaiable images.`);
+//     } // якшо на сторінці відображені всі можливі картнки
+
+//     return refs.gallery.insertAdjacentHTML('beforeend', markup(newImg.hits));
+//   } catch (error) {
 //     throw new Error(
 //       Notiflix.Notify.failure(
 //         'Sorry, there are no images matching your search query. Please try again.'
 //       )
 //     );
-//   });
-
-//   totalImg += newImg.hits.length; // додаємо до загальної к-сті нові дозавантажені картинки
-
-//   if (totalImg === newImg.totalHits) {
-//     refs.moreBtn.hidden = true;
-//     Notiflix.Notify.info(`You have got all avaiable images.`);
-//   } // якшо на сторінці відображені всі можливі картнки
-
-//   return refs.gallery.insertAdjacentHTML('beforeend', markup(newImg.hits));
+//   }
 // }
 
 import Notiflix from 'notiflix';
@@ -75,7 +72,7 @@ import { markup } from './markup';
 import { refs } from './refs';
 
 let page = 1;
-let totalImg = 0; /*  для відсілдковування загальної к-сті картинок, щоб прибрати кнопку load more */
+let totalImg = 0; /*  для відсілдковування загальної к-сті поточних картинок, щоб прибрати кнопку load more */
 
 refs.form.addEventListener('submit', onSubmit);
 refs.moreBtn.addEventListener('click', loadMore);
@@ -118,6 +115,7 @@ function onSubmit(evt) {
 function loadMore() {
   const value = refs.form.elements.searchQuery.value;
   page += 1;
+
   return fetchImg(value, page)
     .then(data => {
       totalImg += data.hits.length; // додаємо до загальної к-сті нові дозавантажені картинки
