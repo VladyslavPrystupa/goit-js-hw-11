5bb51a5b96637eeb2e5da3ad4b7d323c8f4145a3
VladyslavPrystupa <prystupa.vladyslavv@gmail.com>
2023-01-29 00:17:29 +0200

    adaptive styles

--- src/js/markup.js
+++ src/js/markup.js
@@ -10,19 +10,19 @@ export const markup = img => {
         comments,
         downloads,
       }) => `<div class="photo-card">
-      <img src="${webformatURL}" alt="${tags}" loading="lazy" height="240"/>
+  <div class="thumb"><img src="${webformatURL}" alt="${tags}" loading="lazy"/></div>
         <div class="info">
           <p class="info-item">
-            <b>Likes:</b> ${likes}
+            <b>Likes:</b>${likes}
           </p>
           <p class="info-item">
-            <b>Views:</b> ${views}
+            <b>Views:</b>${views}
           </p>
           <p class="info-item">
-            <b>Comments:</b> ${comments}
+            <b>Comments:</b>${comments}
           </p>
           <p class="info-item">
-            <b>Downloads:</b> ${downloads}
+            <b>Downloads:</b>${downloads}
           </p>
         </div>
       </div>`
